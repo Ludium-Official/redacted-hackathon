ed6a4d8bf73c6a8d4d910e1cd2a174642588570e
energyGiver <energy.giver.me@gmail.com>
2024-10-29 18:59:47 +0900

    build: initialize backend setup

---
+++ backend/src/modules/auth/app/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Inject, Req, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ResultResponseDto } from "@src/shared/dtos";
+import { Request } from "express";
+
+import { TokensResponseDto, UserDto } from "../domain/dtos";
+import { AuthService } from "./auth.service";
+
+@Controller("auth")
+export class AuthController {
+    constructor(
+        @Inject("AuthService") private readonly _service: AuthService
+    ) {}
+
+    /**
+     * Handles the Google login request.
+     * This route is protected by the Google authentication guard.
+     * @returns {ResultResponseDto} The result of the login request.
+     *
+     * @tag auth
+     */
+    @Get("google/login")
+    @UseGuards(AuthGuard("google"))
+    handleLogin(): ResultResponseDto {
+        const result = ResultResponseDto.of({
+            result: "Google Authentication"
+        });
+        return result;
+    }
+
+    /**
+     * Handles the Google callback after successful authentication.
+     * This route is protected by the Google authentication guard.
+     * @param {Request} req - The request object.
+     * @returns {Promise<TokensResponseDto>} The tokens response DTO.
+     *
+     * @internal
+     * @tag auth
+     */
+    @Get("google/callback")
+    @UseGuards(AuthGuard("google"))
+    handleRedirect(@Req() req: Request): Promise<TokensResponseDto> {
+        const { user } = req;
+        const result = this._service.googleLogin(UserDto.of(user));
+        return result;
+    }
+}
---
+++ backend/src/modules/user/app/user.controller.ts
@@ -0,0 +1,144 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Inject,
+    Param,
+    Put,
+    UseGuards
+} from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { GetUserId } from "@src/common/decorator/get-user-id.decorator";
+import { AuthGuard } from "@src/common/guard/auth.guard";
+
+import { ProfileBodyDto } from "../domain/dtos";
+import { UserService } from "./user.service";
+
+@Controller("users")
+export class UserController {
+    constructor(@Inject("UserService") private readonly service: UserService) {}
+
+    /**
+     * Health check endpoint.
+     * @returns A Promise that resolves to the health check result.
+     * @throws Throws an error if an error occurs during the health check.
+     *
+     * @tag user
+     */
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    healthCheck(): any {
+        const result = this.service.healthCheck();
+        return result;
+    }
+
+    /**
+     * Get user by ID.
+     * @param id - The ID of the user.
+     * @returns A Promise that resolves to the user with the specified ID.
+     * @throws Throws an error if an error occurs while retrieving the user.
+     *
+     * @tag user
+     */
+    @Get("/:id")
+    @HttpCode(HttpStatus.OK)
+    async getUserById(@Param("id") id: string): Promise<any> {
+        const result = await this.service.getUserById(id);
+        return result;
+    }
+
+    /**
+     * Get user by email.
+     * @param email - The email of the user.
+     * @returns A Promise that resolves to the user with the specified email.
+     * @throws Throws an error if an error occurs while retrieving the user.
+     *
+     * @tag user
+     */
+    @Get("/:email")
+    @HttpCode(HttpStatus.OK)
+    async getUserByEmail(@Param("email") email: string): Promise<any> {
+        const result = await this.service.getUserByEmail(email);
+        return result;
+    }
+
+    /**
+     * Update user profile.
+     * @security bearer
+     * @param id - The ID of the user.
+     * @param userId - The ID of the authenticated user.
+     * @param profile - The updated profile data.
+     * @returns A Promise that resolves to the updated user profile.
+     * @throws Throws an error if an error occurs while updating the user profile.
+     *
+     * @security bearer
+     * @tag user
+     */
+    @Put("/:id/profile")
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.CREATED)
+    async updateUserProfile(
+        @Param("id") id: string,
+        @GetUserId() userId: string,
+        @Body() profile: ProfileBodyDto
+    ): Promise<any> {
+        const result = await this.service.updateUserProfile(
+            id,
+            userId,
+            profile
+        );
+        return result;
+    }
+
+    /**
+     * Delete user.
+     * @security bearer
+     * @param id - The ID of the user to delete.
+     * @param userId - The ID of the authenticated user.
+     * @returns A Promise that resolves to the result of the delete operation.
+     * @throws Throws an error if an error occurs while deleting the user.
+     *
+     * @security bearer
+     * @tag user
+     */
+    @Delete("/:id")
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.OK)
+    async deleteUser(
+        @Param("id") id: string,
+        @GetUserId() userId: string
+    ): Promise<any> {
+        const result = await this.service.deleteUser(id, userId);
+        return result;
+    }
+
+    /**
+     * Message queue health check endpoint.
+     * @returns A Promise that resolves to the health check result.
+     * @throws Throws an error if an error occurs during the health check.
+     *
+     * @internal
+     * @tag user
+     */
+    @Get("mq")
+    @HttpCode(HttpStatus.OK)
+    async mqHealthCheck(): Promise<any> {
+        const result = await this.service.mqHealthCheck();
+        return result;
+    }
+
+    /**
+     * Message queue receiver.
+     * @param data - The received message data.
+     *
+     * @internal
+     * @tag user
+     */
+    @MessagePattern({ cmd: "hello" })
+    mqReceiver(@Payload() data: string) {
+        console.log(data);
+    }
+}
