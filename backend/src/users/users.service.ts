446b35b2fa4c36537cbd7a02806c5a0018320940
energyGiver <energy.giver.me@gmail.com>
2024-11-08 00:12:27 +0900

    feat: reinit backend

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,31 @@
+// src/users/users.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) { }
+
+    findAll(): Promise<User[]> {
+        return this.usersRepository.find();
+    }
+
+    findOne(id: number): Promise<User> {
+        return this.usersRepository.findOneBy({ id });
+    }
+
+    async createUser(name: string, email: string): Promise<User> {
+        const user = this.usersRepository.create({ name, email });
+        return this.usersRepository.save(user);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.usersRepository.delete(id);
+    }
+}
